ae0950cfa72df79f08f4454de748736cedb2f01c
modex9 <modestas.slivinskas@gmail.com>
2019-12-26 23:19:42 +0200

    Admin middleware implemented

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -11,7 +11,7 @@ class ProductsController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('isAdmin');
     }
     /**
      * Display a listing of the resource.
